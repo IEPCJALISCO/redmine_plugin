59cff84b8489bf30e2b8aa7c81694c2b60410bdb
root <root@localhost.localdomain>
2014-02-13 12:48:16 -0600

    Itial commit

---
+++ init.rb
@@ -0,0 +1,7 @@
+Redmine::Plugin.register :iepc do
+  name 'Iepcjalisco plugin'
+  author 'iZam b. <ismael.barragan@iepcjalisco.org.mx>'
+  description ''
+  version '0.0.1'
+  url 'http://wwww.iepcjalisco.org.mx/'
+end
